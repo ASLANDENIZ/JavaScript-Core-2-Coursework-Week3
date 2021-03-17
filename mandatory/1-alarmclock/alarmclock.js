e268b474ffdef05f9871f1ff5af97bdf0c887965
Deniz Kelemence <dkelemence@gmail.com>
2021-03-17 23:32:22 +0000

    homework first one is done!

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,44 @@
-function setAlarm() {}
+
+
+let timeLeft = document.getElementById("timeRemaining");
+
+function setAlarm() {
+  let alarmSetValue = document.getElementById("alarmSet").value;
+  convertTime(alarmSetValue);
+  let countDown = setInterval(() => {
+    alarmSetValue -= 1;
+    console.log(alarmSetValue);
+    convertTime(alarmSetValue);
+    if (alarmSetValue === 0) {
+      playAlarm();
+
+      clearInterval(countDown);
+
+      timeLeft.style.backgroundColor = "red";
+
+
+
+    }
+  }, 1000);
+
+
+}
+
+function convertTime(sec) {
+  let minutes = Math.floor(sec / 60);
+  let seconds = sec % 60;
+  timeLeft.innerHTML = `Time Remaining: ${minutes}:${seconds}`;
+
+}
+
+
+
+
+
+
+
+
+
 
 // DO NOT EDIT BELOW HERE
 
